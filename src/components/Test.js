77153e7bdb615519839a1816f86aa3d2e9b6d3af
Shambhavi-Priya <110760920+Shambhavi-Priya@users.noreply.github.com>
2023-01-27 15:32:42 +0530

    Add files via upload

---
+++ src/components/Test.js
@@ -0,0 +1,40 @@
+import React,{Component} from "react";
+// const Test =()=>{
+//     return (
+//         <div>
+//             <h1>
+//                 hello, my name is Lav Kumar
+//             </h1>
+//         </div>
+//     )
+// }
+
+
+// class Test extends Component{
+//     state={
+//         isloggedin:false
+//     };
+//     render(){
+//         console.log("Hello");
+//         return (
+//           <div>
+//             {!this.state.isloggedin ? (
+//               <h1>you are not logged in</h1>
+//             ) : (
+//               <h1>congrats, you are logged in</h1>
+//             )}
+//             <button onClick={(e) =>this.setState({isloggedin :!this.state.isloggedin})}>Login
+//             </button>
+//           </div>
+//         );
+//     }
+// }
+
+const Test=({user="",setName})=>{
+  return (
+  <>
+    <h1>The name is :{user}</h1>
+    <input type="text" onChange={(e)=> setName(e.target.value)} />
+  </>)
+}
+export default Test;
